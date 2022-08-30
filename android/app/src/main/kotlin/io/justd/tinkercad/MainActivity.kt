4087bbba80dc9a1c839d976f69b9d796be5a655b
defuera <denis@devartel.io>
2022-08-30 17:45:32 +0200

    initial commit

---
+++ android/app/src/main/kotlin/io/justd/tinkercad/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.justd.tinkercad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/features/home/home_screen.dart
@@ -0,0 +1,30 @@
+// MVP, MVC, MVVM
+//
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('I\'m home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '1',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
